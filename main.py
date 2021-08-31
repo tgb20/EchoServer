e63511d1f0a9d5713a6b0ec7b8c0d86f01b2ea62
Jacob Bashista <tgb.bashista@gmail.com>
2021-08-31 15:11:25 -0400

    Update requirements and add some logging

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("port")
 args = parser.parse_args()
 
+print("Starting websocket server")
+
 async def echo(websocket, path):
     async for message in websocket:
         await websocket.send(message)
 
 start_server = websockets.serve(echo, "localhost", args.port)
+print("Started websocket server")
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
